495c7d1c5ec0edf5186b77f0a6aefebe3bf7d98a
Rupam058 <rupam.irp@gmail.com>
2024-04-23 11:42:19 +0530

    changing research hover 2

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ const cross = document.querySelector(".cross");
 menuToggle.addEventListener("click", function () {
   const links = document.querySelector(".links");
   links.classList.toggle("open");
-  
 
   sandwitch.classList.toggle("open");
   cross.classList.toggle("open");
+});
+
+// ---------------------------------------------
+
+const dropdown = document.querySelector(".dropdown");
+const dropdownM = document.querySelector(".dropdown-menu");
+
+dropdown.addEventListener("click", function () {
+  dropdownM.classList.toggle("open");
 });
