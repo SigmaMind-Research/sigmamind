76706c7d36fa7de37bfec2ffe371ab2ed2281b49
Rupam058 <rupam.irp@gmail.com>
2024-04-26 17:48:35 +0530

    landing page 1

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-const menuToggle = document.querySelector(".menu__toggle");
-const links = document.querySelector(".links");
-const sandwitch = document.querySelector(".sandwitch");
-const cross = document.querySelector(".cross");
+function sticky_header() {
+  window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 100);
+  });
+}
 
-menuToggle.addEventListener("click", function () {
-  links.classList.toggle("open");
+function header_dropdown() {
+  const menuToggle = document.querySelector(".menu__toggle");
+  const links = document.querySelector(".links");
+  const sandwitch = document.querySelector(".sandwitch");
+  const cross = document.querySelector(".cross");
 
-  menuToggle.classList.toggle("active");
-});
+  menuToggle.addEventListener("click", function () {
+    links.classList.toggle("open");
 
-// ---------------------------------------------
+    menuToggle.classList.toggle("active");
+  });
 
-const dropdown = document.querySelector(".dropdown");
-const dropdownM = document.querySelector(".dropdown-menu");
+  // ---------------------------------------------
 
-dropdown.addEventListener("click", function () {
-  dropdownM.classList.toggle("open");
-});
+  const dropdown = document.querySelector(".dropdown");
+  const dropdownM = document.querySelector(".dropdown-menu");
+
+  dropdown.addEventListener("click", function () {
+    dropdownM.classList.toggle("open");
+  });
+}
+
+sticky_header();
+header_dropdown();
